109bf1b79855257a3926bf6ed5efd18d8aea058e
biyuqi <biyuqiwan@163.com>
2019-06-21 00:20:28 +0800

    feat: prevent port conflicts & automatic Increase 1 on the basis of the original port

--- command/serve.js
+++ command/serve.js
@@ -1,37 +1,48 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const portfinder = require('portfinder')
 const WebpackDevServer = require('webpack-dev-server')
 const webpackDevConfig = require('../config/dev')
 
-module.exports = (api) => {
-  const  { entry, output, publicPath, frame } = api
+module.exports = async (api) => {
+  const  { entry, output, publicPath, frame, port=8080 } = api
+
+  portfinder.basePort = port
+
+  portfinder.getPort({host:'127.0.0.1'} , (err, port) => {
+    if (err){
+      return;
+    }
+    const uri = 'http://127.0.0.1:' + port
+
+    const ftbDevConfig = webpackDevConfig({
+      entry,
+      output,
+      publicPath,
+      frame,
+      port
+    })
   
-  const ftbDevConfig = webpackDevConfig({
-    entry,
-    output,
-    publicPath,
-    frame
-  })
+    Object.keys(ftbDevConfig.entry).map(item => {
+      ftbDevConfig.entry[item] = [
+        `webpack-dev-server/client?${uri}/`,
+        ftbDevConfig.entry[item]
+      ]
+    })
+  
+    const compiler = webpack(ftbDevConfig)
+    const devServer = new WebpackDevServer(compiler, {
+      contentBase: path.resolve(`${process.cwd()}`, '../dist/index.html'),
+      publicPath: '/',
+      compress: true,
+      noInfo: true,
+      disableHostCheck: true,
+      open: true
+    })
 
-  Object.keys(ftbDevConfig.entry).map(item => {
-    ftbDevConfig.entry[item] = [
-      'webpack-dev-server/client?http://127.0.0.1:8080/',
-      ftbDevConfig.entry[item]
-    ]
-  })
-
-  const compiler = webpack(ftbDevConfig)
-  const devServer = new WebpackDevServer(compiler, {
-    contentBase: path.resolve(`${process.cwd()}`, '../dist/index.html'),
-    publicPath: '/',
-    compress: true,
-    noInfo: true,
-    disableHostCheck: true,
-    open: true
-  })
-
-  devServer.listen(8080, 'localhost', () => {
-    console.log('[FTB-CLI] Starting server on http://localhost:8080')
+    devServer.listen(port, 'localhost', () => {
+      console.log(`[FTB-CLI] Starting server on http://localhost:${port}`)
+    })
   })
 }
