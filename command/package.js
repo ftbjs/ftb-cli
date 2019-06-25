544fa23138606a16f6c1cf44c2cbd920d093309e
BiYuqi <biyuqiwan@163.com>
2019-06-26 00:10:11 +0800

    feat: complated the render package json for react mode

--- command/package.js
+++ command/package.js
@@ -1,5 +1,13 @@
-module.exports = (options) => {
+const path = require('path')
+const { renderTemplate } = require('./utils/index')
+
+const packageJson = (options) => {
+  const { appName, frameName } = options
   return new Promise((resolve, reject) => {
+    const packageTpl = path.resolve(__dirname, `../templates/package/${frameName}.package.json.hbs`)
+    renderTemplate(packageTpl, `${process.cwd()}/${appName}/package.json`, options)
     resolve()
   })
 }
+
+module.exports = packageJson
