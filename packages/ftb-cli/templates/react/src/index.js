cc7d34bbb32095a8a7fb0459536d9cf4f08a0fa0
BiYuqi <biyuqiwan@163.com>
2019-06-30 16:40:36 +0800

    refactor: change default import wrong path

--- packages/ftb-cli/templates/react/src/index.js
+++ packages/ftb-cli/templates/react/src/index.js
@@ -1,5 +1,5 @@
-import React from '../node_modules/react'
-import ReactDom from '../node_modules/react-dom'
+import React from 'react'
+import ReactDom from 'react-dom'
 import App from './App'
 
 ReactDom.render(<App />, document.getElementById('app'))
