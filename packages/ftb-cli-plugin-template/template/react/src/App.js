b2ee1ddfc6a7cd82c9afd5df802ff0a6abf2c831
BiYuqi <biyuqiwan@163.com>
2019-08-16 23:04:05 +1000

    feat: update template for react

--- packages/ftb-cli-plugin-template/template/react/src/App.js
+++ packages/ftb-cli-plugin-template/template/react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Logo from '../public/logo.png'
+import Logo from '../public/logo.png' 
 import './App.scss'
 
 const App = () => {
   return (
     <div className="ftb-cli">
       <img src={Logo} alt="Ftb logo" />
-      <div className="ftb-cli_title">Ftb React Demo</div>
+      <div className="ftb-cli_title">Ftb Cli For React</div>
     </div>
   )
 }
