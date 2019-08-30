cef7bd414dc542d97a19d660fae612bd7770c70a
BiYuqi <biyuqiwan@163.com>
2019-08-31 00:17:41 +0800

    chore: remove useless code

--- packages/ftb-js-package/index.js
+++ packages/ftb-js-package/index.js
@@ -1,16 +1,6 @@
 const createTask = require('./createTask')
 
 module.exports = async (api) => {
-  const { frameName } = api
   const createTaskInstance = new createTask(api)
-  switch (frameName) {
-    case 'js':
-      await createTaskInstance.createJs()
-      break
-    case 'react':
-      await createTaskInstance.createReact()
-      break
-    default:
-      break
-  }
+  await createTaskInstance.createJs()
 }
