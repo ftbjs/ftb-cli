97045926f67b62f41671238c73155d90666bfdfd
BiYuqi <biyuqiwan@163.com>
2019-09-02 18:24:59 +0800

    chore: add webpack-auto-find-port plugin

--- example/react/build/webpack.dev.config.js
+++ example/react/build/webpack.dev.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const portfinder = require('portfinder')
 const chalk = require('chalk')
+const webpackAutoFindPort = require('webpack-auto-find-port')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const webpackBaseConfig = require('./webpack.base.config')
@@ -23,26 +23,9 @@ const webpackDevConfig = merge(webpackBaseConfig, {
   ]
 })
 
-module.exports = async () => {
-  const oldPort = webpackDevConfig.devServer.port
-  portfinder.basePort = oldPort
-
-  portfinder.getPort({host:'127.0.0.1'} , (err, port) => {
-    if (err){
-      console.log(chalk.red(`No port can used. Please check your machine`))
-      return
-    }
-
-    if (port !== oldPort) {
-      console.log(chalk.yellow(`The port ${oldPort} is used. will open the server with new port. \n`))
-    }
-
-    const uri = 'http://127.0.0.1:' + port
-
-    webpackDevConfig.devServer.port = port
-
-    console.log(chalk.green('Project is running at:', uri))
-  })
-
-  return webpackDevConfig
-}
+module.exports = webpackAutoFindPort({
+  config: webpackDevConfig,
+  logger: (port) => {
+    console.log(chalk.green(`Project is Runing at ${port}`))
+  }
+})
